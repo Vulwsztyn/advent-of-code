26164389927a0dfc37609344b29b37d76ecf52eb
Artur Mostowski <artur.mostowski@protonmail.com>
2023-12-05 19:54:32 +0100

    shells for d456

---
+++ day6/src/main.rs
@@ -0,0 +1,26 @@
+use std::env;
+use std::fs;
+
+fn get_file_path() -> String {
+    let args: Vec<String> = env::args().collect();
+
+    let file_path = &args[1];
+
+    file_path.to_string()
+}
+
+fn get_file_contents(file_path: String) -> String {
+    let contents = fs::read_to_string(file_path)
+        .expect("Should have been able to read the file");
+
+    contents
+}
+
+fn main() {
+    let file_path = get_file_path();
+
+    println!("Filename: {}", file_path);
+
+    let contents = get_file_contents(file_path);
+    println!("Contents: {}", contents);
+}
