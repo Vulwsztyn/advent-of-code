edef21ad1e30e577d5cafc4daebf32ef06a61ae6
Artur Mostowski <artur.mostowski@protonmail.com>
2023-12-06 17:42:19 +0100

    day4.1 done

--- day4/src/main.rs
+++ day4/src/main.rs
@@ -1,6 +1,6 @@
 use std::env;
 use std::fs;
-
+use onig::*;
 fn get_file_path() -> String {
     let args: Vec<String> = env::args().collect();
 
@@ -23,6 +23,8 @@ fn main() {
 
     let contents = get_file_contents(file_path);
     let lines = contents.lines().collect::<Vec<&str>>();
+    let number_regex = Regex::new(r"\d+").unwrap();
+    let mut sum = 0;
     lines.iter().enumerate().for_each(
         |(line_i, line)| {
             println!("{} {}", line_i, line);
@@ -31,8 +33,42 @@ fn main() {
             let left_split = left_with_header.split(':').collect::<Vec<&str>>();
             let left = left_split.get(1).unwrap();
             let right = split.get(1).unwrap();
-            println!("{}", left);
-            println!("{}", right);
+
+            println!(" left: {}", left);
+            println!("right: {}", right);
+            
+            let part_to_nums = |part| number_regex
+                .find_iter(part)
+                .map(|(x,x2)| {
+                    part[x..x2].parse::<i32>().unwrap()
+                    }
+                )
+                .collect::<Vec<i32>>();
+
+            let left_nums = part_to_nums(left);
+            let right_nums = part_to_nums(right);
+            // left_nums.iter().for_each(|x| print!("{} ", x));
+            // println!("");
+            // right_nums.iter().for_each(|x| print!("{} ", x));
+            // println!("");
+            let in_both = right_nums
+                .iter()
+                .filter(|x|
+                    left_nums.contains(x)
+                ).collect::<Vec<&i32>>();
+            in_both.iter().for_each(|x| print!("{} ", x));
+            println!("");
+            if in_both.len()==0 {
+                return;
+            }
+            let mut additive = 1;
+            let pow = in_both.len()-1;
+            for _ in 0..pow {
+                additive *= 2;
+            }
+            println!("{}", additive);
+            sum+=additive;
         }
-    )
+    );
+    println!("{}", sum);
 }
