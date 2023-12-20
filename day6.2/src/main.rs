af51114033e6410734446fe87ffd94964dfd5e1c
Artur Mostowski <artur.mostowski@protonmail.com>
2023-12-20 11:04:05 +0100

    day6.2

---
+++ day6.2/src/main.rs
@@ -0,0 +1,64 @@
+use std::env;
+use std::fs;
+use onig::*;
+use std::iter::zip;
+
+fn get_file_path() -> String {
+    let args: Vec<String> = env::args().collect();
+
+    let file_path = &args[1];
+
+    file_path.to_string()
+}
+
+fn get_file_contents(file_path: String) -> String {
+    let contents = fs::read_to_string(file_path)
+        .expect("Should have been able to read the file");
+
+    contents
+}
+
+fn main() {
+    let file_path = get_file_path();
+
+    println!("Filename: {}", file_path);
+
+    let contents = get_file_contents(file_path);
+    let lines = contents.lines().collect::<Vec<&str>>();
+    lines.iter().enumerate().for_each(
+        |(line_i, line)| {
+            println!("{} {}", line_i, line);
+        }
+    );
+    let times = get_numbers(&lines, 0);
+    let limits = get_numbers(&lines, 1);
+    let numbers = zip(times, limits).map(
+        |(time, limit)| {
+            let mut number_of_ways = 0;
+            for velocity in 1..time - 1 {
+                let time_left = time - velocity;
+                let distance = velocity * time_left;
+                if distance > limit {
+                    // println!("{} {} {} {}", time, limit, velocity, time_left);
+                    number_of_ways += 1;
+                }
+            }
+            number_of_ways
+        }
+    ).collect::<Vec<u128>>();
+    println!("{:?}", numbers);
+    let product = numbers.iter().product::<u128>();
+    println!("{}", product);
+}
+
+fn get_numbers(lines: &Vec<&str>, index: usize) -> Vec<u128> {
+    let number_regex = Regex::new(r"\d+").unwrap();
+    let line_without_spaces = lines[index].replace(" ", "");
+    println!("{} {}", &lines[index], line_without_spaces);
+    number_regex.find_iter(&line_without_spaces).map(
+        |(start, end)| {
+            line_without_spaces[start..end].parse::<u128>().unwrap()
+        }
+    ).collect::<Vec<u128>>()
+
+}
