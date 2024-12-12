4f87fe17aea8eaf626021cb215e12f1ad48685b8
Artur Mostowski <artur.mostowski@protonmail.com>
2024-12-12 22:47:11 +0100

    day10.2

--- 2024/day10/main.py
+++ 2024/day10/main.py
@@ -2,19 +2,35 @@ from pathlib import Path
 import re
 
 
-def find_trails(pos, text, value):
+def find_trails_distinct_9s(pos, text, value):
     if 0 > pos[0] or pos[0] >= len(text) or 0 > pos[1] or pos[1] >= len(text[0]):
         return set()
     if text[pos[0]][pos[1]] != str(value):
         return set()
-    print(f"({pos[0]}, {pos[1]}): {value}")
+    # print(f"({pos[0]}, {pos[1]}): {value}")
     if value == 9:
         return set([pos])
     result = set()
-    result |= find_trails((pos[0] - 1, pos[1]), text, value + 1)
-    result |= find_trails((pos[0] + 1, pos[1]), text, value + 1)
-    result |= find_trails((pos[0], pos[1] - 1), text, value + 1)
-    result |= find_trails((pos[0], pos[1] + 1), text, value + 1)
+    result |= find_trails_distinct_9s((pos[0] - 1, pos[1]), text, value + 1)
+    result |= find_trails_distinct_9s((pos[0] + 1, pos[1]), text, value + 1)
+    result |= find_trails_distinct_9s((pos[0], pos[1] - 1), text, value + 1)
+    result |= find_trails_distinct_9s((pos[0], pos[1] + 1), text, value + 1)
+    return result
+
+
+def find_trails_distinct(pos, text, value):
+    if 0 > pos[0] or pos[0] >= len(text) or 0 > pos[1] or pos[1] >= len(text[0]):
+        return 0
+    if text[pos[0]][pos[1]] != str(value):
+        return 0
+    # print(f"({pos[0]}, {pos[1]}): {value}")
+    if value == 9:
+        return 1
+    result = 0
+    result += find_trails_distinct((pos[0] - 1, pos[1]), text, value + 1)
+    result += find_trails_distinct((pos[0] + 1, pos[1]), text, value + 1)
+    result += find_trails_distinct((pos[0], pos[1] - 1), text, value + 1)
+    result += find_trails_distinct((pos[0], pos[1] + 1), text, value + 1)
     return result
 
 
@@ -23,7 +39,16 @@ text = file.read_text().splitlines()
 result = 0
 for i, line in enumerate(text):
     for j, v in enumerate(line):
-        to_add = len(find_trails((i, j), text, 0))
+        to_add = len(find_trails_distinct_9s((i, j), text, 0))
+        # if v == "0":
+        #     print(f"({i}, {j}): {to_add}")
+        result += to_add
+print(result)
+
+result = 0
+for i, line in enumerate(text):
+    for j, v in enumerate(line):
+        to_add = find_trails_distinct((i, j), text, 0)
         # if v == "0":
         #     print(f"({i}, {j}): {to_add}")
         result += to_add
