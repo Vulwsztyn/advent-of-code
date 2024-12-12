5897d8ac138d787227d972881028861c1bc42c37
Artur Mostowski <artur.mostowski@protonmail.com>
2024-12-12 22:44:35 +0100

    day10.1

--- 2024/day10/main.py
+++ 2024/day10/main.py
@@ -1,24 +1,30 @@
 from pathlib import Path
 import re
 
+
+def find_trails(pos, text, value):
+    if 0 > pos[0] or pos[0] >= len(text) or 0 > pos[1] or pos[1] >= len(text[0]):
+        return set()
+    if text[pos[0]][pos[1]] != str(value):
+        return set()
+    print(f"({pos[0]}, {pos[1]}): {value}")
+    if value == 9:
+        return set([pos])
+    result = set()
+    result |= find_trails((pos[0] - 1, pos[1]), text, value + 1)
+    result |= find_trails((pos[0] + 1, pos[1]), text, value + 1)
+    result |= find_trails((pos[0], pos[1] - 1), text, value + 1)
+    result |= find_trails((pos[0], pos[1] + 1), text, value + 1)
+    return result
+
+
 file = Path(__file__).parent / "data.txt"
-text = file.read_text()
+text = file.read_text().splitlines()
 result = 0
-for i, v in enumerate(text.split("\n")):
-    print(i, v)
-    for m in re.findall(r"mul\((\d+),(\d+)\)", v):
-        result += int(m[0]) * int(m[1])
-print(result)
-result = 0
-enabled = True
-for i, v in enumerate(text.split("\n")):
-    print(i, v)
-    for m in re.findall(r"(?:(?:mul\((\d+),(\d+)\))|(do\(\))|(don\'t\(\)))", v):
-        # print(m)
-        if m[3]:
-            enabled = False
-        elif m[2]:
-            enabled = True
-        elif enabled:
-            result += int(m[0]) * int(m[1])
+for i, line in enumerate(text):
+    for j, v in enumerate(line):
+        to_add = len(find_trails((i, j), text, 0))
+        # if v == "0":
+        #     print(f"({i}, {j}): {to_add}")
+        result += to_add
 print(result)
