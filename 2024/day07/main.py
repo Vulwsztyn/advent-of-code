309547adecf24ca01160b98ade6328dc14c9b0b2
Artur Mostowski <artur.mostowski@protonmail.com>
2024-12-12 18:14:02 +0100

    day07

---
+++ 2024/day07/main.py
@@ -0,0 +1,35 @@
+from pathlib import Path
+import re
+
+file = Path(__file__).parent / "data.txt"
+text = file.read_text()
+inputs = []
+result = 0
+for i, v in enumerate(text.split("\n")):
+    if not v:
+        break
+    value, numbers = v.split(":")
+    value = int(value)
+    numbers = [int(x) for x in numbers.strip().split(" ")]
+    inputs.append((value, numbers))
+print(inputs)
+
+result = 0
+for value, numbers in inputs:
+    print(value, numbers)
+    possible = [numbers[0]]
+    for n in numbers[1:]:
+        possible = [x + n for x in possible] + [x * n for x in possible]
+    if value in possible:
+        result += value
+print(result)
+
+result = 0
+for value, numbers in inputs:
+    print(value, numbers)
+    possible = [numbers[0]]
+    for n in numbers[1:]:
+        possible = [x + n for x in possible] + [x * n for x in possible] + [int(str(x)+str(n)) for x in possible]
+    if value in possible:
+        result += value
+print(result)
