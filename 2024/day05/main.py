ff4404622460034925912bd75729cdfac90e05b5
Artur Mostowski <artur.mostowski@protonmail.com>
2024-12-13 11:44:26 +0100

    day5.2

--- 2024/day05/main.py
+++ 2024/day05/main.py
@@ -1,6 +1,28 @@
 from pathlib import Path
 from collections import defaultdict
+from dataclasses import dataclass
 
+@dataclass
+class Node:
+    value: int
+    next: "Node | None" = None
+    previous: "Node | None" = None
+
+    def __len__(self):
+        current = self
+        count = 0
+        while current is not None:
+            count += 1
+            current = current.next
+        return count
+    
+    def get(self, index):
+        current = self
+        for i in range(index):
+            if current is None:
+                return None
+            current = current.next
+        return current
 
 def get_rules_and_updates(text):
     rules = defaultdict(set)
@@ -17,6 +39,16 @@ def get_rules_and_updates(text):
             rules[a].add(b)
     return rules, updates
 
+def print_list(head: Node, limit: int | None = None):
+    current = head
+    count = 0
+    while current is not None:
+        print(current.value, end=" ")
+        count += 1
+        if limit is not None and count >= limit:
+            break
+        current = current.next
+    print()
 
 def is_valid_helper(previous, index, update, rules):
     current = update[index]
@@ -24,7 +56,6 @@ def is_valid_helper(previous, index, update, rules):
     violate = must_be_after.intersection(previous)
     # print(update, previous, current, must_be_after)
     if violate:
-        print(violate)
         return False
     if index == len(update) - 1:
         return True
@@ -35,7 +66,7 @@ def is_valid(update, rules):
     return is_valid_helper(set(), 0, update, rules)
 
 
-file = Path("test.txt")
+file = Path(__file__).parent / "data.txt"
 text = file.read_text().split("\n")
 rules, updates = get_rules_and_updates(text)
 print(rules)
@@ -47,5 +78,52 @@ for update in updates:
         to_add = update[len(update) // 2]
         print(to_add)
         result += to_add
-    print(update, valid)
+        print(update)
+print(result)
+print()
+
+result = 0
+for update in updates:
+    linked_list = Node(update[0])
+    current = linked_list
+    for i in update[1:]:
+        current.next = Node(i)
+        current.next.previous = current
+        current = current.next
+    changed = False
+    current = linked_list
+    while current is not None:
+        current_2 = current.next
+        next_current = current.next
+        previous_current = current.previous
+        changed_this_round = False
+        while current_2 is not None:
+            # print(current.value, current_2.value)
+            if current.value in rules[current_2.value]:
+                changed_this_round = True
+                changed = True
+                next_current2 = current_2.previous
+                current_2.previous.next = current_2.next
+                if current_2.next is not None:
+                    current_2.next.previous = current_2.previous
+                current_2.previous = current.previous
+                current_2.next = current
+                if current.previous is not None:
+                    current.previous.next = current_2
+                else:
+                    linked_list = current_2
+                current.previous = current_2
+                # print_list(linked_list)
+                current_2 = next_current2
+            current_2 = current_2.next
+        if not changed_this_round:
+            current = next_current
+        elif previous_current is not None:
+            current = previous_current.next
+        else:
+            current = linked_list
+    if changed:
+        to_add = linked_list.get(len(update) // 2).value
+        # print(to_add)
+        result += to_add
 print(result)
