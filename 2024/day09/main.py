01cae66a845b0285aa9a4d5d635dfa4837030fb4
Artur Mostowski <artur.mostowski@protonmail.com>
2024-12-14 22:30:03 +0100

    day9.1

--- 2024/day09/main.py
+++ 2024/day09/main.py
@@ -1,24 +1,48 @@
 from pathlib import Path
-import re
+from itertools import chain
 
 file = Path(__file__).parent / "data.txt"
-text = file.read_text()
+text = file.read_text().strip()
+
+is_file = True
+current_id = 0
+file_id = 0
+files = []
+blanks = []
+for i in (int(x) for x in text):
+    if is_file:
+        files.append((current_id, i, file_id))
+        file_id += 1
+    else:
+        blanks.append((current_id, i))
+    current_id += i
+    is_file = not is_file
+print(files)
+print(blanks)
+
+moved = []
+for start, length in blanks:
+    should_break = False
+    while length > 0:
+        last = files.pop()
+        if start > last[0]:
+            files.append(last)
+            should_break = True
+            break
+        moved.append((start, min(length, last[1]), last[2]))
+        if last[1] > length:
+            files.append((last[0], last[1] - length, last[2]))
+        start += last[1]
+        length -= last[1]
+    if should_break:
+        break
+print(moved)
+print(files)
+
 result = 0
-for i, v in enumerate(text.split("\n")):
-    print(i, v)
-    for m in re.findall(r"mul\((\d+),(\d+)\)", v):
-        result += int(m[0]) * int(m[1])
-print(result)
-result = 0
-enabled = True
-for i, v in enumerate(text.split("\n")):
-    print(i, v)
-    for m in re.findall(r"(?:(?:mul\((\d+),(\d+)\))|(do\(\))|(don\'t\(\)))", v):
-        # print(m)
-        if m[3]:
-            enabled = False
-        elif m[2]:
-            enabled = True
-        elif enabled:
-            result += int(m[0]) * int(m[1])
+for start, length, value in chain(files, moved):
+    print(start, length, value)
+    to_add = start * length + (length * (length - 1)) / 2
+    print(to_add)
+    result += to_add * value
 print(result)
