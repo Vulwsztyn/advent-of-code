84f89565ce8b9484d4c9a8ddb2094330fe90d343
Artur Mostowski <artur.mostowski@protonmail.com>
2024-12-15 10:39:59 +0100

    day13.2 wip

--- 2024/day13/main.py
+++ 2024/day13/main.py
@@ -15,37 +15,75 @@ def parse_input(text):
     return result
 
 
-file = Path(__file__).parent / "data.txt"
+def part1(input):
+    result = 0
+    for button_a, button_b, prize in input:
+        best = None
+        for button_a_presses in range(101):
+            if (prize[0] - button_a[0] * button_a_presses) < 0:
+                break
+            if (prize[1] - button_a[1] * button_a_presses) < 0:
+                break
+            if (prize[0] - button_a[0] * button_a_presses) % button_b[0] != 0:
+                continue
+            if (prize[1] - button_a[1] * button_a_presses) % button_b[1] != 0:
+                continue
+            button_b_presses_1 = (
+                prize[0] - button_a[0] * button_a_presses
+            ) // button_b[0]
+            button_b_presses_2 = (
+                prize[1] - button_a[1] * button_a_presses
+            ) // button_b[1]
+            if button_b_presses_1 != button_b_presses_2:
+                continue
+            button_b_presses = button_b_presses_1
+            if button_b_presses >= 101:
+                continue
+            current = button_a_presses * 3 + button_b_presses
+            if best is None or current < best:
+                best = current
+        if best is not None:
+            result += best
+    return result
+
+
+def part2(input):
+    result = 0
+    for button_a, button_b, prize in input:
+        best = None
+        prize = tuple(x + 10000000000000 for x in prize)
+        for button_a_presses in range(button_b[0] * button_b[1]):
+            if (prize[0] - button_a[0] * button_a_presses) < 0:
+                break
+            if (prize[1] - button_a[1] * button_a_presses) < 0:
+                break
+            if (prize[0] - button_a[0] * button_a_presses) % button_b[0] != 0:
+                continue
+            if (prize[1] - button_a[1] * button_a_presses) % button_b[1] != 0:
+                continue
+            button_b_presses_1 = (
+                prize[0] - button_a[0] * button_a_presses
+            ) // button_b[0]
+            button_b_presses_2 = (
+                prize[1] - button_a[1] * button_a_presses
+            ) // button_b[1]
+            if button_b_presses_1 != button_b_presses_2:
+                continue
+            button_b_presses = button_b_presses_1
+
+            current = button_a_presses * 3 + button_b_presses
+            if best is None or current < best:
+                best = current
+        if best is not None:
+            result += best
+    return result
+
+
+file = Path(__file__).parent / "test.txt"
 text = file.read_text().strip().splitlines()
 input = parse_input(text)
 print(input)
-
-result = 0
-for button_a, button_b, prize in input:
-    print(button_a, button_b, prize)
-    best = None
-    for button_a_presses in range(101):
-        if (prize[0] - button_a[0] * button_a_presses) < 0:
-            break
-        if (prize[1] - button_a[1] * button_a_presses) < 0:
-            break
-        if (prize[0] - button_a[0] * button_a_presses) % button_b[0] != 0:
-            continue
-        if (prize[1] - button_a[1] * button_a_presses) % button_b[1] != 0:
-            continue
-        button_b_presses_1 = (prize[0] - button_a[0] * button_a_presses) // button_b[0]
-        button_b_presses_2 = (prize[1] - button_a[1] * button_a_presses) // button_b[1]
-        if button_b_presses_1 != button_b_presses_2:
-            continue
-        button_b_presses = button_b_presses_1
-        if button_b_presses >= 101:
-            continue
-
-        current = button_a_presses * 3 + button_b_presses
-        print(button_a_presses, button_b_presses, current)
-        if best is None or current < best:
-            best = current
-    print(best)
-    if best is not None:
-        result += best
-print(result)
+r1 = part1(input)
+r2 = part2(input)
+print(r1)
+print(r2)
