366abbdc5d620bc28c1068022188a184cc2c2468
Artur Mostowski <artur.mostowski@protonmail.com>
2024-12-16 13:09:03 +0100

    day14.2

--- 2024/day10/main.py
+++ 2024/day10/main.py
@@ -1,5 +1,4 @@
 from pathlib import Path
-import re
 
 
 def find_trails_distinct_9s(pos, text, value):
--- 2024/day14/main.py
+++ 2024/day14/main.py
@@ -1,18 +1,10 @@
 from pathlib import Path
 import re
 from collections import defaultdict
+import numpy
 
-file = Path(__file__).parent / "data.txt"
-width, height = (11, 7) if str(file).endswith("test.txt") else (101, 103)
-text = file.read_text().strip().splitlines()
-robots = []
-for line in text:
-    for m in re.findall(r"(-?\d+),(-?\d+)[^\d-]*(-?\d+),(-?\d+)", line):
-        robots.append(tuple((int(x) for x in (m[0], m[1], m[2], m[3]))))
-print(robots)
-# robots=robots[:1]
-# robots=[(2,4,2,-3)]
-for i in range(100, 101):
+
+def part_1(robots, width, height, i):
     positions = defaultdict(int)
     for r in robots:
         col = (r[0] + r[2] * i) % width
@@ -40,3 +32,45 @@ for i in range(100, 101):
     for i in results:
         result *= i
     print(result)
+
+
+file = Path(__file__).parent / "data.txt"
+width, height = (11, 7) if str(file).endswith("test.txt") else (101, 103)
+text = file.read_text().strip().splitlines()
+robots = []
+for line in text:
+    for m in re.findall(r"(-?\d+),(-?\d+)[^\d-]*(-?\d+),(-?\d+)", line):
+        robots.append(tuple((int(x) for x in (m[0], m[1], m[2], m[3]))))
+print(robots)
+# robots=robots[:1]
+# robots=[(2,4,2,-3)]
+part_1(robots, width, height, 100)
+
+
+min_var_rows = 1e9
+min_var_cols = 1e9
+i_min_var_rows = 0
+i_min_var_cols = 0
+for i in range(104):
+    rows = []
+    cols = []
+    for r in robots:
+        col = (r[0] + r[2] * i) % width
+        row = (r[1] + r[3] * i) % height
+        rows.append(row)
+        cols.append(col)
+    var_cols = numpy.var(cols)
+    var_rows = numpy.var(rows)
+    if min_var_rows > var_rows:
+        min_var_rows = var_rows
+        i_min_var_rows = i
+    if min_var_cols > var_cols:
+        min_var_cols = var_cols
+        i_min_var_cols = i
+print(i_min_var_rows, i_min_var_cols)
+my_i = 0
+for i in range(103):
+    if (i_min_var_rows + height * i) % width == i_min_var_cols:
+        my_i = i
+part_1(robots, width, height, i_min_var_rows + height * my_i)
+print(i_min_var_rows + height * my_i)
