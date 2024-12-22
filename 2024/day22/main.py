75091afedd4402fd137fa9c12e7c042aef527b01
Artur Mostowski <artur.mostowski@protonmail.com>
2024-12-22 09:17:21 +0100

    day22.2 wip

--- 2024/day22/main.py
+++ 2024/day22/main.py
@@ -23,13 +23,46 @@ def next_num(x):
     return x
 
 
-file = Path(__file__).parent / "data.txt"
+def find_sequence(changes, sequence):
+    for i, v in enumerate(changes):
+        if v != sequence[0]:
+            continue
+        if i + len(sequence) > len(changes):
+            break
+        for j in range(1, len(sequence)):
+            if changes[i + j] != sequence[j]:
+                break
+        else:
+            return i + j
+    return None
+
+
+file = Path(__file__).parent / "test.txt"
 text = [int(x) for x in file.read_text().strip().splitlines()]
 print(text)
 result = 0
-for num in text:
-    for i in range(2000):
+prices = []
+changes = []
+for i, num in enumerate(text):
+    # for i,num in enumerate([123]):
+    prices.append([])
+    changes.append([])
+    for j in range(2000):
         # print(num,i)
-        num = next_num(num)
+        m = next_num(num)
+        price = m % 10
+        prices[i].append(price)
+        change = price - (num % 10)
+        changes[i].append(change)
+        num = m
     result += num
+print(result)
+result = 0
+for i, change_seq in enumerate(changes):
+    index = find_sequence(change_seq, [-2, 1, -1, 3])
+    if index is None:
+        continue
+    to_add = prices[i][index]
+    print(i, text[i], to_add)
+    result += to_add
 print(result)
